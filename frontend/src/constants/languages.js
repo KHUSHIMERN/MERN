de1177adff9bb1f5cb072e89fbaf1a488e92b514
Arpita Pandey <arpitapandey9142@gmail.com>
2026-07-28 13:49:05 +0530

    Delete frontend directory

--- frontend/src/constants/languages.js
+++
@@ -1,7 +0,0 @@
-export const LANGUAGES = [
-  { code: 'en', name: 'English', nativeName: 'English', flag: '🇬🇧' },
-  { code: 'hi', name: 'Hindi', nativeName: 'हिन्दी', flag: '🇮🇳' },
-  { code: 'kn', name: 'Kannada', nativeName: 'ಕನ್ನಡ', flag: '🇮🇳' },
-];
-
-export default LANGUAGES;
